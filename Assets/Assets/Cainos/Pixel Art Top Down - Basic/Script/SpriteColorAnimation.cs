43e6e9ffe517c370b5459fdc6ff6d82e68816469
José Enrique Cerezo <joseenrique61@hotmail.com>
2025-05-24 12:19:20 -0500

    Avance completo

---
+++ Assets/Assets/Cainos/Pixel Art Top Down - Basic/Script/SpriteColorAnimation.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    //animate the sprite color base on the gradient and time
+    public class SpriteColorAnimation : MonoBehaviour
+    {
+        public Gradient gradient;
+        public float time;
+
+        private SpriteRenderer sr;
+        private float timer;
+
+        private bool playerInRange = false;
+
+        private AudioSource audioSource;
+
+        private void Start()
+        {
+            timer = time * Random.value;
+            sr = GetComponent<SpriteRenderer>();
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private void Update()
+        {
+            if (playerInRange)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
+
+            if (timer > time) return;
+			if (timer < 0) timer = 0.0f;
+
+			sr.color = gradient.Evaluate(timer / time);
+        }
+
+		private void OnTriggerEnter2D(Collider2D collision)
+		{
+			if (collision.CompareTag("Player"))
+            {
+                playerInRange = true;
+                audioSource.PlayOneShot(audioSource.clip);
+            }
+		}
+
+		private void OnTriggerExit2D(Collider2D collision)
+		{
+			if (collision.CompareTag("Player"))
+            {
+                playerInRange = false;
+            }
+		}
+	}
+}
---
+++ Assets/Assets/Cainos/Pixel Art Top Down - Basic/Script/TopDownCharacterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    public class TopDownCharacterController : MonoBehaviour
+    {
+        public float speed;
+
+        private Animator animator;
+
+        private AudioSource audioSource;
+
+        [SerializeField] private AudioClip[] audioClips;
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            audioSource = GetComponent<AudioSource>();
+        }
+
+
+        private void Update()
+        {
+            Vector2 dir = Vector2.zero;
+            if (Input.GetKey(KeyCode.A))
+            {
+                dir.x = -1;
+                animator.SetInteger("Direction", 3);
+                PlaySound();
+			}
+			else if (Input.GetKey(KeyCode.D))
+			{
+				dir.x = 1;
+				animator.SetInteger("Direction", 2);
+                PlaySound();
+			}
+
+			if (Input.GetKey(KeyCode.W))
+			{
+				dir.y = 1;
+				animator.SetInteger("Direction", 1);
+                PlaySound();
+			}
+			else if (Input.GetKey(KeyCode.S))
+            {
+                dir.y = -1;
+                animator.SetInteger("Direction", 0);
+                PlaySound();
+            }
+
+            dir.Normalize();
+            animator.SetBool("IsMoving", dir.magnitude > 0);
+
+            GetComponent<Rigidbody2D>().velocity = speed * dir;
+        }
+
+        private void PlaySound()
+        {
+            if (!audioSource.isPlaying)
+				audioSource.PlayOneShot(audioClips[Random.Range(0, audioClips.Length)]);
+        }
+    }
+}
